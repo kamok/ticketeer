06a8e210756f55d3e1f029e1a10d1591dcd69088
Ka Mok <heykamok@gmail.com>
2016-05-05 01:39:30 -0400

    Add update Projects feature.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -24,6 +24,23 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+
+    if @project.save
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash[:notice] = "Project has not been updated."
+      render "edit"
+    end
+  end
+
   private
 
   def project_params
