d77bb92eda331839b5a4477a882d239eb1932bde
Ka Mok <heykamok@gmail.com>
2016-05-08 21:13:27 -0400

    Implement signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+
   root "projects#index"
 
   resources :projects do
     resources :tickets
   end
+
+  resources :users
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
 end
