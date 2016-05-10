0469d1f622c2bf783ef419796b2fbed8824615f0
Ka Mok <heykamok@gmail.com>
2016-05-10 18:40:51 -0400

    Add inability of users below authorization status to create project.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,13 @@ module ApplicationHelper
       end
     end
   end
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def admins_only(&block)
+    block.call if current_user.try(:admin?)
+  end
 end
 
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -22,10 +22,10 @@ describe ProjectsController do
       update: :put,
       destroy: :delete }.each do |action, method|
       it "cannot access the #{action} action" do
-      sign_in(user)
-      send(method, action, :id => FactoryGirl.create(:project))
-      expect(response).to redirect_to(root_path)
-      expect(flash[:alert]).to eql("You must be an admin to do that.")
+        sign_in(user)
+        send(method, action, :id => FactoryGirl.create(:project))
+        expect(response).to redirect_to(root_path)
+        expect(flash[:alert]).to eql("You must be an admin to do that.")
       end
     end
   end
